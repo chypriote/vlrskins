e5260ca5eb2aaf86a6d75701112d25dec73e55b6
NicolasT <nicolastem@hotmail.fr>
2022-06-08 00:07:52 +0200

    start skins grid

---
+++ components/SkinsGrid/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="skins-list">
+		<div class="skin" v-for="skin of skins" :key="skin.id">
+			<div class="frame --standard">
+				<div class="card-pic">
+
+				</div>
+				{{ skin?.attributes?.name }}
+				{{ skin?.attributes }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	skins: Array
+})
+</script>
+
+<style scoped>
+.skins-list {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+.skin {
+	flex: 0 0 25%;
+	max-width: 25%;
+	padding: .25rem;
+}
+.frame {
+	background-color: #223843;
+	min-height: 100%;
+	position: relative;
+	outline-style: solid;
+	outline-offset: 0;
+	outline-width: 3px;
+	cursor: pointer;
+	&.--standard {outline-color: #fff;}
+	&.--select {outline-color: #409dd7;}
+	&.--deluxe {outline-color: #f79f11;}
+	&.--premium {outline-color: #9941b0;}
+	&.--ultra {outline-color: red;}
+}
+</style>
--- pages/weapon/[slug].vue
+++ pages/weapon/[slug].vue
@@ -9,7 +9,7 @@ const route = useRoute()
 const { slug } = route.params
 
 const client = useStrapiClient()
-const strapi: ApiResponse<Skin> = await client<Skin>('/skins', { params: { "filters[weapon][slug][$eq]": slug }})
+const strapi = await client<ApiResponse<Skin>>('/skins', { params: { "filters[weapon][slug][$eq]": slug, populate: 'picture' }})
 const skins = strapi.data
 </script>
 
@@ -26,17 +26,8 @@ const skins = strapi.data
 					<div class="query">
 						<input type="text" placeholder="Type something" />
 					</div>
-					<div class="skins-list">
-						<div class="skin" v-for="skin of skins" :key="skin.id">
-							<div class="frame --standard">
-								<div class="card-pic">
-
-								</div>
-								{{ skin?.attributes?.name }}
-							</div>
-						</div>
-					</div>
 				</div>
+				<skins-grid :skins="skins" />
 			</section>
 		</div>
 	</div>
@@ -88,29 +79,5 @@ section {
 		border-radius: 0.25rem;
 		transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
 	}
-}
-.skins-list {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-}
-.skin {
-	flex: 0 0 25%;
-	max-width: 25%;
-	padding: .25rem;
-}
-.frame {
-	background-color: #223843;
-	min-height: 100%;
-	position: relative;
-	outline-style: solid;
-	outline-offset: 0;
-	outline-width: 3px;
-	cursor: pointer;
-	&.--standard {outline-color: #fff;}
-	&.--select {outline-color: #409dd7;}
-	&.--deluxe {outline-color: #f79f11;}
-	&.--premium {outline-color: #9941b0;}
-	&.--ultra {outline-color: red;}
 }
 </style>
